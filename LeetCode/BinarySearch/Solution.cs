974052c044b9b110303db0c00dcd0ce5acee4c66
tongchenGithub <1471080924@qq.com>
2022-08-01 19:37:53 +0800

    Update
    提交二分法和深度优先search

---
+++ LeetCode/BinarySearch/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography;
+
+namespace BinarySearch
+{
+    public class Solution
+    {
+        bool IsBadVersion(int version)
+        {
+            return false;
+        }
+        public int FirstBadVersion(int n)
+        {
+            var begin = 0;
+            var end = n - 1;
+            while (begin <= end)
+            {
+                var index = (end - begin) / 2 + begin;
+                if (IsBadVersion(index + 1))
+                {
+                    if ((index) == 0 || !IsBadVersion(index))
+                    {
+                        return index + 1;
+                    }
+                    end = index - 1;
+                }
+                else
+                {
+                    begin = index + 1;
+                }
+            }
+
+            return end;
+        }
+
+        //--------------------------------------------------------------------------------------------
+        public int Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+
+            return MergeCombine(target, nums, 0, nums.Length - 1);
+        }
+
+        private int MergeCombine(int target, int[] nums, int begin, int end)
+        {
+            while (begin <= end)
+            {
+                var index = (end - begin) / 2 + begin;
+                if (nums[index] > target)
+                {
+                    end = index - 1;
+                }
+                else if (nums[index] == target)
+                {
+                    return index;
+                }
+                else
+                {
+                    begin = index + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/DFS/Solution.cs
@@ -0,0 +1,71 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+namespace DFS
+{
+    public class TreeNode { 
+        public int val;
+        public TreeNode left;
+        public TreeNode right; 
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+        }
+    }
+    
+    public class Solution {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            var ret = new List<double>();
+            // 开始遍历
+            ret.Add(root.val);
+            RecurveCalcChildren(new List<TreeNode>() { root }, ref ret);
+
+            return ret;
+        }
+
+        private void RecurveCalcChildren(List<TreeNode> nodes, ref List<double> resultList)
+        {
+            var count = 0;
+            double totalNum = 0;
+
+            var nodeList = new List<TreeNode>();
+                
+            foreach (var node in nodes)
+            {
+                if (node.left != null)
+                {
+                    totalNum += node.left.val;
+                    count++;
+                    nodeList.Add(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    totalNum += node.right.val;
+                    count++;
+                    nodeList.Add(node.right);
+                }
+            }
+
+            if (count > 0)
+            {
+                resultList.Add(totalNum * 1.0 / count);
+
+                RecurveCalcChildren(nodeList, ref resultList);
+            }
+        }
+    }
+
+}
