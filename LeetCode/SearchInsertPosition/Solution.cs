0969d0c2f4fdf430eb9b4f3d091ff7974110bfe5
tongchenGithub <1471080924@qq.com>
2022-08-05 19:37:50 +0800

    feature: 【LeetCode】二分法

---
+++ LeetCode/SearchInsertPosition/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace SearchInsertPosition
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var result = nums.Length;
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                var mid = (right - left) / 2 + left;
+                if (target <= nums[mid])
+                {
+                    right = mid - 1;
+                    result = mid;
+                }
+                else if (target > nums[mid])
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
