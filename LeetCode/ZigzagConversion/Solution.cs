099ff6d2f8dc324873ba2e0f502ff317f7bce45f
tongchenGithub <1471080924@qq.com>
2022-08-08 10:58:52 +0800

    feature: 【LeetCode】队列

---
+++ LeetCode/ZigzagConversion/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace DepthFirstSearch.ZigzagConversion
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            var ans = new char[s.Length];
+
+            var arr = s.ToCharArray();
+            var n = arr.Length / numRows;
+            var charList = new char[numRows, 2 * n - 1];
+            ans[0] = arr[0];
+            ans[1] = arr[2 * numRows - 2];
+            ans[2] = arr[2 * (2 * numRows - 2)];
+            ans[3] = arr[3 * (2 * numRows - 2)];
+            ans[4] = arr[4 * (2 * numRows - 2)];
+            ans[5] = arr[1 + 0 * numRows];
+            ans[6] = arr[1 + 1 * numRows];
+            return ans.ToString();
+        }
+    }
+}
---
+++ LeetCode/滑动窗口的最大值/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace 滑动窗口的最大值
+{
+    public class Solution
+    {
+        /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param num int整型一维数组 
+     * @param size int整型 
+     * @return int整型一维数组
+     */
+        public List<int> maxInWindows(List<int> num, int size)
+        {
+            // write code here
+            var len = num.Count;
+            var array = new List<int>(len - (size - 1));
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                queue.Enqueue(num[i]);
+            }
+
+            var maxFirst = 0;
+            foreach (var val in queue)
+            {
+                if (val > maxFirst)
+                {
+                    maxFirst = val;
+                }
+            }
+
+            array.Add(maxFirst);
+
+            for (int i = size; i < len; i++)
+            {
+                queue.Dequeue();
+                queue.Enqueue(num[i]);
+
+                var max = 0;
+                foreach (var val in queue)
+                {
+                    if (val > max)
+                    {
+                        max = val;
+                    }
+                }
+
+                array.Add(max);
+            }
+
+            return array;
+        }
+    }
+}
